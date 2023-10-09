f196d7e931dba740ec8b26ad641b1f8269e24802
liron vaknin <45284793+lironv@users.noreply.github.com>
2023-10-09 10:14:31 +0300

    Update creationscript.sh

--- creationscript.sh
+++ creationscript.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-#appConfigName=runnerconfig
+appConfigName=runnerconfig
 
-#az account set --subscription 98eb592d-460e-4f2a-a0fd-958deefc49e0
-#az group create --name resourcegroup --location eastus
-#vmpassword=$(openssl rand -base64 10)
-#sed -i -e "s/<<DUMMYVALUE>>/$vmpassword/" ./bastion.parameters.json
-#az deployment group create --resource-group resourcegroup --template-file storageaccount2.json --parameters saparam.parameters.json
-#az deployment group create --resource-group resourcegroup --template-file storageaccount2.json --parameters sa2param.parameters.json
-#az deployment group create --resource-group resourcegroup --template-file linuxtemplate.json --parameters bastion.parameters.json
-#az vm show --resource-group resourcegroup --name bastion1 --show-details --query "publicIps" --output tsv >> login.ini
+az group create --name resourcegroup --location eastus
+vmpassword=$(openssl rand -base64 10)
+sed -i -e "s/<<DUMMYVALUE>>/$vmpassword/" ./bastion.parameters.json
+az deployment group create --resource-group resourcegroup --template-file storageaccount2.json --parameters saparam.parameters.json
+az deployment group create --resource-group resourcegroup --template-file storageaccount2.json --parameters sa2param.parameters.json
+az deployment group create --resource-group resourcegroup --template-file linuxtemplate.json --parameters bastion.parameters.json
 storageaccount1pw=$(az storage account keys list --account-name sastorage01projectmsc  --query '[0].value' --output tsv)
 storageaccount2pw=$(az storage account keys list --account-name sastorage02projectmsc  --query '[0].value' --output tsv)
 storageaccount1name="sastorage01"
@@ -22,6 +20,3 @@ sed -i -e "s/<<DUMMYVALUEPASSWORD2>>/${escappedpasswordvalue2}/" ./connect.sh
 
 
 az vm run-command invoke -g resourcegroup -n bastion1 --command-id RunShellScript --scripts "@connect.sh" --parameters $storageaccount1name $storageaccount2name 
-
-#sshpass -p Bastion5key1992 scp connect.sh passer.sh  Bastion@172.171.236.136:/home/Bastion
-#sshpass -p Bastion5key1992 ssh Bastion@172.171.236.136 'sudo apt-get update; sudo apt-get install azure-cli -y; az account set --subscription 98eb592d-460e-4f2a-a0fd-958deefc49e0; /home/Bastion/connect.sh ;sudo /home/Bastion/passer.sh '
