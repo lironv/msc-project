f7adaa07de3e39989e241037a97c42a91a7dbcaa
liron vaknin <45284793+lironv@users.noreply.github.com>
2023-10-10 17:20:56 +0300

    Update connect.sh

--- connect.sh
+++ connect.sh
@@ -1,72 +1,45 @@
 
-
-
-
-
-
-
-
-
-
-
-
 storageaccount1pw="<<DUMMYVALUE>>"
 storageaccount2pw="<<DUMMYVALUE>>"
 mntdir01="/mnt/${1}fs-share"
 mntdir02="/mnt/${2}fs-share"
 source_dir="/mnt/blobs"
 
+function mountfilesharestorageaccount() {
+    #arguments: storageaccount name, storageaccount password, mount directory, 
+    sudo mkdir $3
+    if [ ! -d "/etc/smbcredentials" ]; then
+    sudo mkdir /etc/smbcredentials
+    fi
+    if [ ! -f "/etc/smbcredentials/${1}projectmsc.cred" ]; then
+        sudo bash -c "echo \"username=${1}projectmsc\" >> /etc/smbcredentials/${1}projectmsc.cred"
+        sudo bash -c "echo \"password=${2}\" >> /etc/smbcredentials/${1}projectmsc.cred"
+    fi
+    sudo chmod 600 /etc/smbcredentials/${1}projectmsc.cred
+    sudo bash -c "echo \"//${1}projectmsc.file.core.windows.net/${1}fs-share ${3} cifs nofail,credentials=/etc/smbcredentials/${1}projectmsc.cred,dir_mode=0777,file_mode=0777,serverino,nosharesock,actimeo=30\" >> /etc/fstab"
+    sudo mount -t cifs //${1}projectmsc.file.core.windows.net/${1}fs-share ${3} -o credentials=/etc/smbcredentials/${1}projectmsc.cred,dir_mode=0777,file_mode=0777,serverino,nosharesock,actimeo=30
+}
 
-sudo mkdir $mntdir01
-if [ ! -d "/etc/smbcredentials" ]; then
-sudo mkdir /etc/smbcredentials
-fi
-if [ ! -f "/etc/smbcredentials/${1}projectmsc.cred" ]; then
-    sudo bash -c "echo \"username=${1}projectmsc\" >> /etc/smbcredentials/${1}projectmsc.cred"
-    sudo bash -c "echo \"password=${storageaccount1pw}\" >> /etc/smbcredentials/${1}projectmsc.cred"
-fi
-sudo chmod 600 /etc/smbcredentials/${1}projectmsc.cred
-
-sudo bash -c "echo \"//${1}projectmsc.file.core.windows.net/${1}fs-share ${mntdir01} cifs nofail,credentials=/etc/smbcredentials/${1}projectmsc.cred,dir_mode=0777,file_mode=0777,serverino,nosharesock,actimeo=30\" >> /etc/fstab"
-sudo mount -t cifs //${1}projectmsc.file.core.windows.net/${1}fs-share ${mntdir01} -o credentials=/etc/smbcredentials/${1}projectmsc.cred,dir_mode=0777,file_mode=0777,serverino,nosharesock,actimeo=30
-
-
-sudo mkdir $mntdir02
-if [ ! -d "/etc/smbcredentials" ]; then
-sudo mkdir /etc/smbcredentials
-fi
-if [ ! -f "/etc/smbcredentials/${2}projectmsc.cred" ]; then
-    sudo bash -c "echo \"username=${2}projectmsc\" >> /etc/smbcredentials/${2}projectmsc.cred"
-    sudo bash -c "echo \"password=${storageaccount2pw}\" >> /etc/smbcredentials/${2}projectmsc.cred"
-fi
-sudo chmod 600 /etc/smbcredentials/${2}projectmsc.cred
-
-sudo bash -c "echo \"//${2}projectmsc.file.core.windows.net/${2}fs-share ${mntdir02} cifs nofail,credentials=/etc/smbcredentials/${2}projectmsc.cred,dir_mode=0777,file_mode=0777,serverino,nosharesock,actimeo=30\" >> /etc/fstab"
-sudo mount -t cifs //${2}projectmsc.file.core.windows.net/${2}fs-share ${mntdir02} -o credentials=/etc/smbcredentials/${2}projectmsc.cred,dir_mode=0777,file_mode=0777,serverino,nosharesock,actimeo=30
+mountfilesharestorageaccount $storageaccount1name $storageaccount1pw $mntdir01
+mountfilesharestorageaccount $storageaccount2name $storageaccount2pw $mntdir02
 
 # create and pass files to sa
-
-
 mkdir $source_dir
 cd $source_dir
 
 for ((i=1; i<=100; i++)); do
-    touch "blob$i.txt"
+    sudo touch "blob$i.txt"
 done
 
-for file in "$source_dir"/*; do
-    if [ -f "$file" ]; then
-        filename=$(basename "$file")  
-        mv "$file" "$mntdir01/$filename"
-    fi
-done
-
-for file in "$mntdir01"/*; do
-    if [ -f "$file" ]; then
-       filename=$(basename "$file")      
-       mv "$file" "$mntdir02/$filename"
-    fi
-done
-
-
+function movefilestosharestorageaccount() {
+# arguments: source directory, target directory
+    for file in "$1"/*; do
+        if [ -f "$file" ]; then
+           filename=$(basename "$file")      
+           sudo mv "$file" "$2/$filename"
+        fi
+    done
+}
+movefilestosharestorageaccount $source_dir $mntdir01
+movefilestosharestorageaccount $mntdir01 $mntdir02
 
